524ddc876076403cfcc40d9077000ab1467ef922
capulongarvin@gmail.com <acapulong@skytv.co.nz>
2019-10-09 15:16:17 +1300

    initial commit

---
+++ src/PracticeOne.java
@@ -0,0 +1,20 @@
+public class PracticeOne {
+    public static void main(String[] args) {
+        System.out.println("hello world from ARVIN the Tester");
+        System.out.println("Schar_max = " + Short.MAX_VALUE);
+        int myInteger;
+        myInteger = 100;
+        System.out.println("This is the value of My Integer ="+ myInteger);
+        int myProduct;
+        myProduct = myInteger *10;
+        System.out.println("This is the value of My Product = " + myProduct);
+        double myDouble = 100.3456;
+        System.out.println("This is my Double = " + myDouble);
+        float myFloat = 324.56f;
+        System.out.println("This is My FLoat = "+ myFloat);
+    }
+}
+
+
+
+
